f03562cca1fe651f50d28003f0e27e60bf0f0c66
JNjenga <james.njenga@strathmore.edu>
2020-05-01 17:58:18 +0300

    Initial project setup

---
+++ premake5.lua
@@ -0,0 +1,68 @@
+workspace 'VisualWorkspace'
+	location 'workspace'
+	architecture 'x86_64'
+	language 'C++'
+
+	configurations { "Debug32", "Release32", "Debug64", "Release64" }
+
+	filter "configurations:*32"
+		architecture "x86"
+
+	filter "configurations:*64"
+		architecture "x86_64"
+	
+	filter {'configurations:Debug*'}
+		symbols 'On'
+	filter {'configurations:Release*'}
+		optimize 'On'
+	filter {}
+
+	targetdir 'bin'
+
+	objdir 'bin/Obj'
+
+	cppdialect 'C++11'
+
+-- Include Glad
+include '3rdParty/Glad'
+include '3rdParty/Glfw'
+
+project 'Visual'
+	kind 'ConsoleApp'
+	language 'C++'
+
+	includedirs{
+		'3rdParty/Glfw/include',
+		'3rdParty/Glad/include',
+		'3rdParty/glm/include',
+	}
+
+	pchheader 'src/vis_pch.hpp'
+
+	files {
+		'src/**.cpp',
+		'src/**.hpp',
+		'src/**.h'
+	}
+
+	links {
+		'GlfwStatic3.3'.
+		'Glad'
+	}
+
+	filter {'system:windows'}
+		links {'Opengl32'}
+	filter{}
+
+	filter {'system:not windows'}
+	links {
+		"GL",
+		"dl",
+		"pthread",
+		"X11",
+		"Xxf86vm",
+		"Xrandr",
+		"Xi",
+		"Xinerama",
+		"Xcursor",
+	}
---
+++ readme.md
@@ -0,0 +1,27 @@
+Visualize :
+
+- Audio Files (.mp3)
+- Algorithms
+- Graphs
+- Wave functions
+
+And more
+
+Tech used : Opengl & Vulkan
+
+### How to run
+
+`audvis data.mp3`
+`audvis data.csv`
+
+### How build
+
+ - Generate project files using premake
+Run the premake script
+`premake5 vs2017` for windows or `premake5 gmake2`
+
+The solution/make files will be in the `workspace` directory.
+
+### Lisence
+
+CCO 
